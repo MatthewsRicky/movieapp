e41308162bcd2e58f86effc797b4f694115de3cb
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-01-28 16:06:08 +0300

    adding the hero image to the header
    Signed-off-by: MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>

--- movieapp/src/App.jsx
+++ movieapp/src/App.jsx
@@ -6,8 +6,11 @@ const App = () => {
             <div className="pattern" />
             <div className="wrapper">
                 <header>
+                    <img src="./hero.png" alt="Hero Banner" />
                     <h1>Find <span className="text-gradient">movies</span> you&apos;ll enjoy without the hassle</h1>
                 </header>
+
+                <p>search </p>
             </div>
         </main>
     )
