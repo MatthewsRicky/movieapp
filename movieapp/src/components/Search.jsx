464e625abd5f4cbfbb8b9c65e5101a8b00c5006f
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-01-28 16:46:35 +0300

    fully functional and styled search bar
    Signed-off-by: MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>

--- movieapp/src/components/Search.jsx
+++ movieapp/src/components/Search.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 
-const Search = () => {
+const Search = ({searchTerm, setSearchTerm}) => {
     return (
-        <div className="text-white text-3xl">Search</div>
+        <div className="search">
+            <div>
+                <img src="search.svg" alt="search" />
+
+                <input
+                    type="search"
+                    placeholder="Search thousands of movies"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}/>
+            </div>
+        </div>
     )
 }
 export default Search
