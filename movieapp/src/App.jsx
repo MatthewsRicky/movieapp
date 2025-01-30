ba76586614d20aff09357ab4d5b5d1260ce2e18f
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-01-30 22:42:50 +0300

    fixing the base url definition,
    declarion and definition of movielist,
    
    error handling the api response
    
    Signed-off-by: MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>

--- movieapp/src/App.jsx
+++ movieapp/src/App.jsx
@@ -1,7 +1,7 @@
 import Search from "./components/Search.jsx";
 import {useEffect, useState} from "react";
 
-const API_BASE_URL = "https://api.themoviedb.org/3/discover/movie";
+const API_BASE_URL = "https://api.themoviedb.org/3/";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
 const API_OPTIONS = {
@@ -17,15 +17,37 @@ const App = () => {
 
     const [errorMessage, setErrorMessage] = useState("");
 
+    const [movieList, setMovieList] = useState([])
+
+    const [loading, setLoading] = useState(false)
+
     const fetchMovies = async () => {
         try {
-            const response = await fetch(API_BASE_URL, {})
+
+            const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+            const response = await fetch(endpoint, API_OPTIONS);
+
+            if (!response.ok) {
+                throw new Error("Could not fetch movies");
+            }
+
+            const data = await response.json();
+
+            if(data.response === 'False'){
+                setErrorMessage(data.error || "Failed to fetch movies.");
+            }
+
+            setMovieList(data.results || []);
+
         } catch (error){
             console.log(error)
         }
     }
 
-    useEffect(() => {}, [])
+    useEffect(() => {
+        fetchMovies();
+    }, [])
 
     return (
         <main>
