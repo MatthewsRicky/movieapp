774c9830d0b52bd679c8525682480f8233b2fb34
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-01-31 10:05:47 +0300

    factorizing out the movi card into a component
    Signed-off-by: MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>

--- movieapp/src/App.jsx
+++ movieapp/src/App.jsx
@@ -1,6 +1,7 @@
 import Search from "./components/Search.jsx";
 import {useEffect, useState} from "react";
 import Spinner from "./components/Spinner.jsx";
+import MovieCard from "./components/MovieCard.jsx";
 
 
 const API_BASE_URL = "https://api.themoviedb.org/3/discover";
@@ -71,7 +72,7 @@ const App = () => {
                 </header>
 
                 <section className="all-movies">
-                    <h2 className="mt-[20px]">All Movies</h2>
+                    <h2 className="mt-[40px]">All Movies</h2>
 
                     {loading ? (
                         <Spinner />
@@ -80,7 +81,7 @@ const App = () => {
                     ) : (
                         <ul>
                             {movieList.map((movie) => (
-                                <p key={movie.id} className="text-white">{movie.title}</p>
+                                <MovieCard key={movie.id} movie={movie}/>
                             ))}
                         </ul>
                     )}
