5b201f7d878fce1eeab0684019336a5656acd118
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-01-31 10:43:58 +0300

    Movie card completed. (This is a good example of a representational component. It doesn't handle logic but instead accepts props and renders them to the client. )
    Signed-off-by: MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>

--- movieapp/src/components/MovieCard.jsx
+++ movieapp/src/components/MovieCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const MovieCard = ({ movie:
-    { title, vote_average, poster_path, release_date, original_languagr } }) => {
+    { title, vote_average, poster_path, release_date, original_language } }) => {
     return (
         <div className="movie-card">
             <img
@@ -15,7 +15,16 @@ const MovieCard = ({ movie:
                 <div className="content">
                     <div className="rating">
                         <img src="star.svg" alt="star icon" />
+                        <p>{vote_average ? vote_average.toFixed(1) : "N/A"}</p>
                     </div>
+
+                    <span>•</span>
+                    <p className="lang">{original_language}</p>
+
+                    <span>•</span>
+
+                    <p className="year">{release_date ? release_date.split('-') : 'N/A'}</p>
+
                 </div>
             </div>
         </div>
