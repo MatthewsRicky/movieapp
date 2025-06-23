0217a56f28adc5647ee7d12f49698d40d29cfe8c
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-06-23 22:19:01 +0300

    formatting

--- movieapp/src/components/Search.jsx
+++ movieapp/src/components/Search.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
+import React from "react";
+import PropTypes from "prop-types";
 
-const Search = ({searchTerm, setSearchTerm}) => {
-    return (
-        <div className="search">
-            <div>
-                <img src="search.svg" alt="search" />
+const Search = ({ searchTerm, setSearchTerm }) => {
+  return (
+    <div className="search">
+      <div>
+        <img src="search.svg" alt="search" />
 
-                <input
-                    type="search"
-                    placeholder="Search thousands of movies"
-                    value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)}/>
-            </div>
-        </div>
-    )
-}
-export default Search
+        <input
+          type="search"
+          placeholder="Search thousands of movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+    </div>
+  );
+};
+
+Search.propTypes = {
+  searchTerm: PropTypes.string.isRequired,
+  setSearchTerm: PropTypes.func.isRequired,
+  // or PropTypes.string if it's optional
+};
+export default Search;
